02bba582dc38abfaeaba9622e464707d5f7077d8
卢德智 <453711091@qq.com>
2024-01-05 17:29:07 +0800

    代码提交

---
+++ Todo.Services/User/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Core;
+
+public class UserService
+{
+    private readonly DataContext _context;
+
+    public UserService(DataContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// 获取全部用户数据
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<UserEntity>> GetAsync()
+    {
+        var data = await _context.User.ToListAsync();
+        return data;
+    }
+}
